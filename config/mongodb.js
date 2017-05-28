269ad17e84bcf80d229f8a9a79898c90d49579e8
Max Gardner <max.c.gardner@gmail.com>
2017-05-27 23:04:19 -0500

    Adding correct URI for Mongo Labs

---
+++ config/mongodb.js
@@ -0,0 +1,3 @@
+module.exports = {
+  uri: "mongodb://heroku_2mcf4spp:h44qkds5rt767bqkbjutuasdnp@ds155961.mlab.com:55961/heroku_2mcf4spp"
+}
--- config/mongokey.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  key: "mongolab-encircled-50248"
-}
